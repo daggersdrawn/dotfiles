731b6cc6714c84af626f9166e6eceaf669cea354
Thomas Schreiber <tom@insatsu.us>
2011-04-06 19:46:58 +0200

    tweaks

--- bash/main.sh
+++ bash/main.sh
@@ -2,7 +2,6 @@ export DOTFILES=$HOME/dotfiles
 
 . "$DOTFILES/bash/env.sh"
 . "$DOTFILES/bash/config.sh"
-. "$DOTFILES/base/aliases.sh"
 . "$DOTFILES/bash/aliases.sh"
 
 if [ -f "$HOME/.private" ]; then
