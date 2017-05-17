e2a6c6f6e7f9adb07c62a9362eaa78d437c25429
Thomas Schreiber <rizumu@users.noreply.github.com>
2017-05-17 09:08:19 +0200

    Repair environment variable and PATH handling.

--- bash/env.sh
+++ bash/env.sh
@@ -1,65 +1,53 @@
-### Set path {{{
-
 # add local bin
-if [ -d "/usr/local/bin" ]; then
-    PATH="/usr/local/bin:$PATH"
-fi
+if [ -d "/usr/local/bin" ]; then PATH="/usr/local/bin:$PATH"; fi
 
 # add local sbin
-if [ -d "/usr/local/sbin" ]; then
-    PATH="/usr/local/sbin:$PATH"
+if [ -d "/usr/local/sbin" ]; then PATH="/usr/local/sbin:$PATH"; fi
+
+# add user's private bin
+if [ -d "$HOME/bin" ]; then PATH="$HOME/bin:$PATH"; fi
+
+# add haskell's cabal bin
+if [ -d "$HOME/.cabal/bin" ]; then PATH="$HOME/.cabal/bin:$PATH"; fi
+
+# add pipsi's bin
+if [ -d "$HOME/.local/bin" ]; then PATH="$HOME/.local/bin:$PATH"; fi
+
+# add go's bin
+if [ -d "/usr/local/opt/go/libexec/bin" ]; then PATH="/usr/local/opt/go/libexec/bin:$PATH"; fi
+
+# add ec2's bin
+if [ -d "$HOME/.ec2/" ]; then
+  export EC2_HOME=$HOME/.ec2
+  export PATH=$PATH:$EC2_HOME/bin
+  export EC2_PRIVATE_KEY=`ls $EC2_HOME/pk-*.pem`
+  export EC2_CERT=`ls $EC2_HOME/cert-*.pem`
+  export JAVA_HOME=/usr
 fi
 
-# add user's private bin if it exists
-if [ -d "$HOME/bin" ]; then
-    PATH="$HOME/bin:$PATH"
+if $_islinux; then
+  # add perl's bin
+  export PERL_LOCAL_LIB_ROOT="$HOME/perl5"
+  export PERL_MB_OPT="--install_base $HOME/perl5"
+  export PERL_MM_OPT="INSTALL_BASE=$HOME/perl5"
+  export PERL5LIB="$HOME/perl5/lib/perl5/x86_64-linux-thread-multi:$HOME/perl5/lib/perl5"
+  export PATH="$HOME/perl5/bin:$PATH"
+else
+  # add tex's bin
+  export PATH=/usr/texbin:$PATH
+  # add homebrew's bins
+  export PATH=/usr/local/bin:$PATH
+  export PATH=/usr/local/sbin:$PATH
+  # add gnu coreutils bins
+  export PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
+  export PATH="/usr/local/opt/findutils/libexec/gnubin:$PATH"
+  # add npm's bin
+  export PATH="$HOME/.npm-packages/bin:$PATH"
+  # add m-cli binary to path
+  export PATH=$PATH:/usr/local/m-cli  # https://github.com/rgcr/m-cli
 fi
 
-# add haskell cabal bin if it exists
-if [ -d "$HOME/.cabal/bin" ]; then
-    PATH="$HOME/.cabal/bin:$PATH"
-fi
-
-# add ruby's bins if they exist
-if [ -d "$HOME/.gem/ruby/1.9.1/bin" ]; then
-    PATH="$HOME/.gem/ruby/1.9.1/bin:$PATH"
-fi
-if [ -d "$HOME/.rbenv/bin" ]; then
-    PATH="$HOME/.rbenv/bin:$PATH"
-fi
-
-# Load RVM into a shell session *as a function*
-if [ -s "$HOME/.rvm/scripts/rvm" ]; then
-    . "$HOME/.rvm/scripts/rvm"
-fi
-# }}}
-
-
-### General options {{{
-
-# is $1 installed?
-_have() { which "$1" &>/dev/null; }
-
-# python pyenv
-if which pyenv > /dev/null; then eval "$(pyenv init -)"; fi
-
-# python virtualenv
-export VIRTUALENVWRAPPER_PYTHON=`which python`
-export VIRTUALENVWRAPPER_VIRTUALENV=`which virtualenv`
-source /usr/local/bin/virtualenvwrapper.sh
-
-# # PATH for python
-# PATH=/usr/local/share/python:${PATH}
-# # for homebrewed bottles
-# PATH=/usr/local/bin:/usr/local/sbin:${PATH}
-# # for custom scripts
-# PATH=$HOME/bin:${PATH}
-# for pyenv
-# PYENV_ROOT=$HOME/.pyenv
-# PATH=$PYENV_ROOT/bin:${PATH}
-
-# history
-# Save and reload the history after each command finishes
+# history save and reload after each command finishes
 unset HISTFILESIZE
 unset PROMPT_COMMAND
 export HISTSIZE=100000
@@ -70,148 +58,21 @@ export HISTCONTROL=ignoredups:erasedups:ignorespace
 export PROMPT_COMMAND="history -a; history -c; history -r; $PROMPT_COMMAND"
 shopt -s histappend
 
-# Setting for the new UTF-8 terminal support in Leopard
-export LANG=en_US.UTF-8
-export LC_CTYPE=en_US.UTF-8
-export LC_ALL=en_US.UTF-8
-
-export WERKZEUG_DEBUG_PIN="off"
-
-if [ -d "$HOME/.ec2/" ]; then
-  export EC2_HOME=$HOME/.ec2
-  export PATH=$PATH:$EC2_HOME/bin
-  export EC2_PRIVATE_KEY=`ls $EC2_HOME/pk-*.pem`
-  export EC2_CERT=`ls $EC2_HOME/cert-*.pem`
-  export JAVA_HOME=/usr
-fi
-
-# bash 4 features
-if [[ ${BASH_VERSINFO[0]} -ge 4 ]]; then
-  shopt -s globstar
-  shopt -s autocd
-fi
-
-shopt -s checkwinsize
-shopt -s extglob
-
-# should've done this a long time ago
-set -o emacs
-
-# no flow control outside of the dumb tty
-if [[ "$TERM" != 'linux' ]]; then
-  stty -ixon -ixoff
-fi
-
-# list of apps to be tried in order
-xbrowsers="browser:uzbl-browser:chromium:firefox"
-browsers="elinks:lynx:links"
-editors="subl:emacsclient --nw:emacs:zile:vim:vi"
-
-# }}}
-
-# set $EDITOR
-_set_editor() {
-  local IFS=':' editor
-
-  for editor in $editors; do
-  editor="$(which $editor 2>/dev/null)"
-
-    if [[ -x "$editor" ]]; then
-      export EDITOR="$editor"
-      export VISUAL="$EDITOR"
-      break
-    fi
-  done
-}
-_set_editor
-
-# set ip address
-[[ -f "$HOME/.myip" ]] && export MYIP=$(cat "$HOME/.myip")
-
+# less paging
 export LESS="-QR"
 export PAGER=less
 
-# git
-if [ -f $HOME/.git-completion.bash ]; then
-  . $HOME/.git-completion.bash
-fi
-
-# mercurial
-if [ -f $HOME/.hgtab-bash.sh ]; then
-  . $HOME/.hgtab-bash.sh
-fi
-
-# vim
-if [ -f /usr/share/vim/vim70 ]; then
-  export VIMRUNTIME=/usr/share/vim/vim70
-fi
-if [ -f /usr/share/vim/vim71 ]; then
-  export VIMRUNTIME=/usr/share/vim/vim71
-fi
-if [ -f /usr/share/vim/vim72 ]; then
-  export VIMRUNTIME=/usr/share/vim/vim72
-fi
-
-# dmenu options
-if _have dmenu; then
-  . "$HOME/.dmenurc"
-fi
-
-
-### System conditionals {{{
-# For OSX gcc issues see:
-#     http://stackoverflow.com/questions/8473066/gcc-4-2-failed-with-exit-status-1
-#     https://github.com/kennethreitz/osx-gcc-installer/downloads
-#
-_islinux=false
-[[ "$(uname -s)" =~ Linux|GNU|GNU/* ]] && _islinux=true
-
-_isarch=false
-[[ -f /etc/arch-release ]] && _isarch=true
-
-_isxrunning=false
-[[ -n "$DISPLAY" ]] && _isxrunning=true
-
-_isroot=false
-[[ $UID -eq 0 ]] && _isroot=true
-
-# set $BROWSER
-_set_browser() {
-  local IFS=':' _browsers="$*" browser
-
-  for browser in $_browsers; do
-    browser="$(which $browser 2>/dev/null)"
-
-    if [[ -x "$browser" ]]; then
-      export BROWSER="$browser"
-      break
-    fi
-  done
-}
-$_isxrunning && _set_browser "$xbrowsers" || _set_browser "$browsers"
-
-# command line completion scripts of common Python packages.
-# pip install pycompletion into system python
-[[ `which pycompletion` ]] && source `which pycompletion`
+# no flow control outside of the dumb tty
+if [[ "$TERM" != 'linux' ]]; then stty -ixon -ixoff; fi
 
 if $_islinux; then
   export LS_OPTIONS="--color=auto"
   export PACMAN="pacman"
-else  # assume osx
-  # latex/auctex
-  export PATH=/usr/texbin:$PATH
-  # homebrew paths
-  export PATH=/usr/local/bin:$PATH
-  export PATH=/usr/local/sbin:$PATH
-  export PATH="$HOME/.npm-packages/bin:$PATH"
-  # coreutils fix
-  export PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
-  export PATH="/usr/local/opt/findutils/libexec/gnubin:$PATH"
-  alias rquicksilver="sudo umount -Af && killall Quicksilver && open /Applications/Quicksilver.app"
-  alias killdash="defaults write com.apple.dashboard mcx-disabled -boolean YES; killall Dock;"
-  alias startdash="defaults write com.apple.dashboard mcx-disabled -boolean NO; killall Dock;"
-  alias o="open ."
-  alias o.="open ."
+else
+  # For OSX gcc issues see:
+  #     http://stackoverflow.com/questions/8473066/gcc-4-2-failed-with-exit-status-1
+  #     https://github.com/kennethreitz/osx-gcc-installer/downloads
+
   if [ -f `brew --prefix`/etc/bash_completion ]; then
     . `brew --prefix`/etc/bash_completion
   fi
--- bash/main.sh
+++ bash/main.sh
@@ -1,9 +1,21 @@
-export DOTFILES=$HOME/dotfiles
+# additional shell options
+# http://www.gnu.org/software/bash/manual/html_node/The-Shopt-Builtin.html
+shopt -s globstar
+shopt -s autocd
+shopt -s checkwinsize
+shopt -s extglob
 
-. "$DOTFILES/bash/env.sh"
-. "$DOTFILES/bash/config.sh"
-. "$DOTFILES/bash/aliases.sh"
+# dotfile conditionals
+_have() { which "$1" &>/dev/null; }  # is $1 installed?
+_islinux=false; [[ "$(uname -s)" =~ Linux|GNU|GNU/* ]] && _islinux=true
+_isarch=false; [[ -f /etc/arch-release ]] && _isarch=true
+_isxrunning=false; [[ -n "$DISPLAY" ]] && _isxrunning=true
+_isroot=false; [[ $UID -eq 0 ]] && _isroot=true
 
-if [ -f "$HOME/.private" ]; then
-    . "$HOME/.private"
-fi
+# bash configuration
+. "$HOME/dotfiles/bash/env.sh"
+. "$HOME/dotfiles/bash/config.sh"
+. "$HOME/dotfiles/bash/aliases.sh"
+
+# private env vars
+if [ -f "$HOME/.private" ]; then . "$HOME/.private"; fi
