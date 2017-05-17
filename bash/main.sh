5b9830f5e2634e922e7083aecc5c2555f4b5cfc5
Thomas Schreiber <rizumu@users.noreply.github.com>
2017-05-17 09:40:27 +0200

    Tell the shell to understand emacs editing commands.

--- bash/main.sh
+++ bash/main.sh
@@ -1,3 +1,6 @@
+# tell the shell to understand emacs editing commands
+set -o emacs
+
 # additional shell options
 # http://www.gnu.org/software/bash/manual/html_node/The-Shopt-Builtin.html
 shopt -s globstar
